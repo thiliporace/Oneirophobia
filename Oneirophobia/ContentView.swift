baf270a174fc48316bd73235a181fe9753314563
Thiago Liporace <liporacethiago@gmail.com>
2023-06-12 17:50:29 -0300

    Initial Commit

---
+++ Oneirophobia/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Oneirophobia
+//
+//  Created by Thiago Liporace on 12/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Oneirophobia/OneirophobiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneirophobiaApp.swift
+//  Oneirophobia
+//
+//  Created by Thiago Liporace on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct OneirophobiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
